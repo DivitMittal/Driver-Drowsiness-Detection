1bdbfb47649d6bf5b2c491f202db0ddc48479984
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-04-18 07:05:37 +0530

    feat: add nix & flake support with devshells & formatters

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "DDD flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {flake-parts, ...} @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import inputs.systems;
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        inputs.devshell.flakeModule
+      ];
+
+      perSystem = {pkgs, ...}: {
+        devshells.default = {
+          packages = builtins.attrValues {
+            inherit(pkgs)
+            uv
+            ;
+          };
+        };
+
+        treefmt = {
+          flakeCheck = false;
+          programs = {
+            #typos.enable = true;
+            ## Nix
+            alejandra.enable = true;
+            deadnix.enable = true;
+            statix.enable = true;
+            ## JSON
+            prettier.enable = true;
+            ## Python
+            black.enable = true;
+          };
+          projectRootFile = "flake.nix";
+        };
+      };
+    };
+}
